ffdbc2e9575701a7e9057fbd32af132cf2110863
Olivia Li <oliviali.ouyang@gmail.com>
2022-02-09 01:32:08 -0800

    add tailwind

--- app/frontend/components/App.jsx
+++ app/frontend/components/App.jsx
@@ -1,7 +1,11 @@
 import React from "react";
 
 const App = () => {
-  return <div>TEMPLATE</div>;
+  return (
+    <div className="flex justify-center align-middle">
+      <h1 className="text-2xl">TEMPLATE</h1>
+    </div>
+  );
 };
 
 export default App;
